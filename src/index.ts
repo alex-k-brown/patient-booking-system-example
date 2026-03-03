019af3cd902c55b12a87d6a7090a44d4f6d1c47e
Alex Brown <brown.alex.k@gmail.com>
2026-03-03 10:15:40 -0700

    feat: added patients endpoint

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import providersRouter from "./routes/providers";
 import appointmentsRouter from "./routes/appointments";
+import patientsRouter from "./routes/patients";
 
 const app = express();
 const PORT = 3000;
@@ -9,6 +10,7 @@ const PORT = 3000;
 app.use(express.json());
 app.use("/api/providers", providersRouter);
 app.use("/api/appointments", appointmentsRouter);
+app.use("/api/patients", patientsRouter);
 
 app.get("/", (req, res) => {
   res.send("Patient Booking API");
